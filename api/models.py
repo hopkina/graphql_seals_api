5f9eb60d8512a5e682d1ba0ea828574ebd98aaea
hopkina <alisonhopkin@yahoo.com>
2021-12-23 07:23:26 +0000

    change data model

--- api/models.py
+++ api/models.py
@@ -2,17 +2,22 @@ from main import db
 
 
 class Site(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    site_id = db.Column(db.Integer, primary_key=True)
+    site_name = db.Column(db.String)
     x_coord = db.Column(db.Integer)
     y_coord = db.Column(db.Integer)
-    description = db.Column(db.String)
-    date_updated = db.Column(db.Date)
+    management_area = db.Column(db.String)
+    seal_species = db.Column(db.String)
+    comments = db.Column(db.String)
+    date_from = db.Column(db.Date)
 
     def to_dict(self):
         return {
-            "id": self.id,
+            "site_id": self.site_id,
+            "site_name": self.site_name,
             "x_coord": self.x_coord,
             "y_coord": self.y_coord,
-            "description": self.description,
-            "date_updated": str(self.date_updated.strftime('%d-%m-%Y'))
+            "management_area": self.management_area,
+            "comments": self.comments,
+            "date_from": str(self.date_from.strftime('%d-%m-%Y'))
         }
--- api/mutations.py
+++ api/mutations.py
@@ -7,14 +7,26 @@ from api.models import Site
 
 
 @convert_kwargs_to_snake_case
-def resolve_create_site(obj, info, x_coord, y_coord, description, date_updated):
+def resolve_create_site(obj, 
+    info, 
+    site_name, 
+    x_coord, y_coord, 
+    management_area, 
+    seal_species, 
+    comments, 
+    date_from):
+    """ create a new site """
+
     try:
-        date_updated = datetime.strptime(date_updated, '%d-%m-%Y').date()
+        date_from = datetime.strptime(date_from, '%d-%m-%Y').date()
         site = Site(
+            site_name=site_name,
             x_coord=x_coord, 
-            y_coord=y_coord, 
-            description=description, 
-            date_updated=date_updated
+            y_coord=y_coord,
+            management_area=management_area,
+            seal_species=seal_species,
+            comments=comments,
+            date_from=date_from
         )
         db.session.add(site)
         db.session.commit()
@@ -22,6 +34,7 @@ def resolve_create_site(obj, info, x_coord, y_coord, description, date_updated):
             "success": True,
             "site": site.to_dict()
         }
+
     except ValueError:  # date format errors
         payload = {
             "success": False,
@@ -33,11 +46,11 @@ def resolve_create_site(obj, info, x_coord, y_coord, description, date_updated):
 
 
 @convert_kwargs_to_snake_case
-def resolve_update_location(obj, info, site_id, x_coord, y_coord):
+def resolve_update_location(obj, info, site_id, new_x_coord, new_y_coord):
     try:
         site = Site.query.get(site_id)
-        site.x_coord = x_coord
-        site.y_coord = y_coord
+        site.x_coord = new_x_coord
+        site.y_coord = new_y_coord
         db.session.add(site)
         db.session.commit()
         payload = {
@@ -64,7 +77,7 @@ def resolve_delete_site(obj, info, site_id):
     except AttributeError:
         payload = {
             "success": False,
-            "errors": [f"site matching id {site_id} not found"]
+            "errors": [f"Site matching id {site_id} not found"]
         }
 
     return payload
